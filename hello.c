8e3b55eb1839f205fefe12ada7f3aff10a8bfef6
okidesu <okidesu69@gmail.com>
2022-05-12 16:27:49 +0800

    fixed comment character added a line at the end

--- hello.c
+++ hello.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-# zob
 
 
-
-# nucubun comment 1
-# tinker y va de son comment
+// nucubun comment 1
+// tinker y va de son comment
 int main( int argc,const char* argv[] )
 {
 	printf( "hello world from tinker !\n" );
 
 	return 0;	
 }
+
+
+// change 1
+// change 2
 
 
